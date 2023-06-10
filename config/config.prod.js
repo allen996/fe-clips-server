5df4cfd05468e560d2a47b20379decb63491634a
allen996 <1964621612@qq.com>
2023-06-10 16:56:34 +0800

    test

--- config/config.prod.js
+++ config/config.prod.js
@@ -1,9 +1,15 @@
-config.sequelize = {
-  dialect: 'mysql',
-  host: process.env.db_host,
-  port: process.env.db_port,
-  database: 'fe-clips',
-  username: process.env.db_username,
-  password: process.env.db_pwd,
-  timezone: '+08:00'
-};
+module.exports = appInfo => {
+  const config = exports = {};
+
+  config.sequelize = {
+    dialect: 'mysql',
+    host: process.env.db_host,
+    port: process.env.db_port,
+    database: 'fe-clips',
+    username: process.env.db_username,
+    password: process.env.db_pwd,
+    timezone: '+08:00'
+  };
+
+  return config
+}
